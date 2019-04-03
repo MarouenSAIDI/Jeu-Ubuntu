927323f1c47f08d80d202b0336e43cf719e1f77f
nourbenamor <47628829+nourbenamor@users.noreply.github.com>
2019-04-03 18:56:20 +0200

    Create objet.h

---
+++ objet.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+
+
+#ifndef FONCTION_H_
+#define FONCTION_H_
+ 
+
+
+SDL_BlitSurface(imageDeFond, NULL, ecran, &positionFond);
+void pause ();
+
+
+ 
+#endif
