c93ff5e9518ba97527783dc46a61daca6d0844db
nourbenamor <47628829+nourbenamor@users.noreply.github.com>
2019-04-03 13:35:59 +0100

    Create souris.c

---
+++ souris.c
@@ -0,0 +1,71 @@
+include "fonction.h"
+
+switch(event.type)
+
+{
+
+   case SDL_QUIT:
+
+       continuer = 0;
+
+       break;
+
+   case SDL_MOUSEBUTTONUP: 
+
+       break;
+
+}
+switch(event.type)
+
+{
+
+   case SDL_QUIT:
+
+       continuer = 0;
+
+       break;
+
+   case SDL_MOUSEBUTTONUP:
+
+       if (event.button.button == SDL_BUTTON_RIGHT) 
+
+           continuer = 0;
+
+       break;
+
+}
+while (continuer)
+
+{
+
+   SDL_WaitEvent(&event);
+
+   switch(event.type)
+
+   {
+
+       case SDL_QUIT:
+
+           continuer = 0;
+
+           break;
+
+       case SDL_MOUSEBUTTONUP:
+
+           positionperso.x = event.button.x;
+
+           positionperso.y = event.button.y;
+
+           break;
+
+   }
+
+
+
+   SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
+
+   SDL_BlitSurface(perso, NULL, ecran, &positionperso); 
+
+   SDL_Flip(ecran);
+
+}
