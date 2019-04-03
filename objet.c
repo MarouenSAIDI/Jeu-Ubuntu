435439386bf36d5e12642c5a1b4b315c02f140d1
nourbenamor <47628829+nourbenamor@users.noreply.github.com>
2019-04-03 17:35:22 +0100

    Create objet.c

---
+++ objet.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+void pause();
+ 
+int main(int argc, char *argv[])
+{
+    SDL_Surface *ecran = NULL, *imageDeFond = NULL, *objet = NULL;
+    SDL_Rect positionFond, positionobjet;
+ 
+    positionFond.x = 0;
+    positionFond.y = 0;
+    positionobjet.x = 500;
+    positionobjet.y = 260;
+ 
+    SDL_Init(SDL_INIT_VIDEO);
+ 
+    SDL_WM_SetIcon(IMG_Load("sdl_icone.bmp"), NULL);
+ 
+    ecran = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE);
+    SDL_WM_SetCaption("Chargement d'images en SDL", NULL);
+ 
+    imageDeFond = IMG_Load("backg.bmp");
+    SDL_BlitSurface(imageDeFond, NULL, ecran, &positionFond);
+ 
+    
+    objet = IMG_Load("objet.png");
+    SDL_BlitSurface(objet, NULL, ecran, &positionobjet);
+ 
+    SDL_Flip(ecran);
+    pause();
+ 
+    SDL_FreeSurface(imageDeFond);
+    SDL_FreeSurface(objet);
+    SDL_Quit();
+ 
+    return EXIT_SUCCESS;
+}
+ 
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+ 
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
+}
