1052f07b10338ed7d958496b967d811034cfe0f7
nourbenamor <47628829+nourbenamor@users.noreply.github.com>
2019-04-08 21:54:24 +0200

    Update collisionbb.c

--- collisionbb.c
+++ collisionbb.c
@@ -12,3 +12,34 @@ if (box1.x+box1.w<=box2.x)
 return 0;
 return 1;
 }
+int collision(Player hero,enemy e)
+{
+int collision=1;
+if(hero.positiontext.x + hero.positiontext. w =< e.positionenemyy. x 
+|| hero.positiontext.x => e.positionenemyy. x + e.positionenemyy. w
+|| hero.positiontext.y + hero.positiontext.h =< e.positionenemyy. y 
+|| hero.positiontext.y => e.positionenemyy. y + e.positionenemyy. h )
+{
+collision=0;
+}
+else{
+collision=1;
+}
+return collision;
+}
+
+int collisionbox(Player hero,objet ob)
+{
+int collision=1;
+if(hero.positiontext.x + hero.positiontext. w =< ob.positionobjett. x 
+|| hero.positiontext.x => ob.positionobjett. x + ob.positionobjett. w
+|| hero.positiontext.y + hero.positiontext.h =< ob.positionobjett. y 
+|| hero.positiontext.y => ob.positionobjett. y + ob.positionobjett. h )
+{
+collision=0;
+}
+else{
+collision=1;
+}
+return collision;
+}
